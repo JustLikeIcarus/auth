71e8df2de3ad2fa40c4eba6e0cc586be1a7e4091
Kenny <justlikeicarus@gmail.com>
2010-05-19 08:06:22 -0700

    checking for issue with late static binding

--- classes/auth.php
+++ classes/auth.php
@@ -1,3 +1,36 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
-abstract class Auth extends Kohana_Auth { }
+abstract class Auth extends Kohana_Auth {
+
+protected static $instances;
+
+	/**
+	 * Singleton pattern
+	 *
+	 * @return Auth
+	 */
+	public static function instance()
+	{
+		if ( ! isset(self::$instances))
+		{
+echo "instances not set";
+			// Load the configuration for this type
+			$config = Kohana::config('auth');
+
+			if ( ! $type = $config->get('driver'))
+			{
+				$type = 'ORM';
+			}
+
+			// Set the session class name
+			$class = 'Auth_'.ucfirst($type);
+
+			// Create a new session instance
+			self::$instances = new $class($config);
+		}
+ echo Kohana::debug(self::$instances);
+		return self::$instances;
+	}
+
+
+ }
